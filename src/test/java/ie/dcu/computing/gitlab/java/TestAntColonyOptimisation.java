6086574915579828a11a24ae2e3be40d97b35e33
clearyk6 <kevin.cleary6@mail.dcu.ie>
2020-02-16 09:47:41 +0000

    initial changes to matrix

--- src/test/java/ie/dcu/computing/gitlab/java/TestAntColonyOptimisation.java
+++ src/test/java/ie/dcu/computing/gitlab/java/TestAntColonyOptimisation.java
@@ -5,16 +5,16 @@ import org.junit.jupiter.api.*;
 
 
 public class TestAntColonyOptimisation {
-    private AntColonyOptimisation testACO = new AntColonyOptimisation(50);
-
-    @Test
-    void preTourTest() {
-        Assert.assertTrue("Best tour length is null", testACO.bestTourOrder == null);
-    }
-
-    @Test
-    void postTourTest() {
-        testACO.startOptimization();
-        Assert.assertFalse("Best tour length is not null",testACO.bestTourOrder == null);
-    }
+//    private AntColonyOptimisation testACO = new AntColonyOptimisation(50);
+//
+//    @Test
+//    void preTourTest() {
+//        Assert.assertTrue("Best tour length is null", testACO.bestTourOrder == null);
+//    }
+//
+//    @Test
+//    void postTourTest() {
+//        testACO.startOptimization();
+//        Assert.assertFalse("Best tour length is not null",testACO.bestTourOrder == null);
+//    }
 }
